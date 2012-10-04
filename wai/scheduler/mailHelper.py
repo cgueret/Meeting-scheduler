b709c8fc077d8168ad44c8f8439864d13f4eba70
Laurens Rietveld <laurens.rietveld@gmail.com>
2012-10-04 13:44:07 +0200

    added default values to mail helper

--- wai/scheduler/mailHelper.py
+++ wai/scheduler/mailHelper.py
@@ -15,7 +15,7 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
-def mailWai(subject, message, sender, to, cc, replyTo):
+def mailWai(subject, message, sender, to, cc = "", replyTo = ""):
     msg = EmailMultiAlternatives(subject, strip_tags(message), sender, to, [], headers = {'Reply-To': replyTo, 'Cc': cc})
     msg.attach_alternative(message, "text/html")
     msg.send()
